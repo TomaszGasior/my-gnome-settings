7c1d45c827129ece8e52143a1b1b006239c3847e
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2019-05-30 10:24:09 +0200

    Update terminal setting

--- gnome-basic-settings.sh
+++ gnome-basic-settings.sh
@@ -86,6 +86,7 @@ gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/or
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/ name 'System Monitor'
 
 echo "Terminal"
+gsettings set org.gnome.Terminal.Legacy.Settings default-show-menubar false
 gsettings set org.gnome.Terminal.Legacy.Settings theme-variant 'dark'
 gsettings set org.gnome.Terminal.ProfilesList list "['b1dcc9dd-5262-4d8d-a863-c897e6d979b9']"
 gsettings set org.gnome.Terminal.ProfilesList default 'b1dcc9dd-5262-4d8d-a863-c897e6d979b9'
