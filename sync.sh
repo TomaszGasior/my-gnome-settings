d232ac1daa9a3855ed341f05660616131fc5c5d8
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2019-06-09 23:29:39 +0200

    Update sync script

--- sync.sh
+++ sync.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-if [[ $(diff gtk.css ~/.config/gtk-3.0/gtk.css) ]]; then
-    gio trash ~/.config/gtk-3.0/gtk.css
-    cp --verbose gtk.css ~/.config/gtk-3.0/
-fi
+function sync_two_files()
+{
+    changes_from_first=`diff $1 $2 | grep -E "^<"`
+    changes_from_second=`diff $1 $2 | grep -E "^>"`
 
-if [[ $(diff gnome-shell.css ~/.config/gnome-shell/gnome-shell.css) ]]; then
-    gio trash ~/.config/gnome-shell/gnome-shell.css
-    cp --verbose gnome-shell.css ~/.config/gnome-shell/
-fi
+    if [[ "$changes_from_first" && "$changes_from_second" ]]; then
+        echo "BOTH CHANGED: $1 <-> $2"
+    elif [[ "$changes_from_first" ]]; then
+        gio trash $2
+        cp --verbose $1 $2
+    elif [[ "$changes_from_second" ]]; then
+        gio trash $1
+        cp --verbose $2 $1
+    fi
+}
+
+sync_two_files ./gtk.css ~/.config/gtk-3.0/gtk.css
+sync_two_files ./gnome-shell.css ~/.config/gnome-shell/gnome-shell.css
