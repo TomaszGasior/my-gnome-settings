f7223bf2dee9242557d86d1c63a571083fb43ff0
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2019-06-23 19:04:21 +0200

    Rename script file

--- gnome.php
+++ gnome.php
@@ -3,5 +3,5 @@
 header('Content-type: application/x-shellscript; charset=UTF-8');
 header('X-Robots-Tag: noindex');
 
-echo file_get_contents('https://raw.githubusercontent.com/TomaszGasior/my-gnome-settings/master/gnome-basic-settings.sh');
+echo file_get_contents('https://raw.githubusercontent.com/TomaszGasior/my-gnome-settings/master/my-gnome-settings.sh');
 
