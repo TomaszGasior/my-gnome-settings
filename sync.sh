8c540b9670ed3abfdacde4a678421c2f89eede1b
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2019-06-24 11:38:50 +0200

    Update sync script

--- sync.sh
+++ sync.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
-function sync_two_files()
-{
-    changes_from_first=`diff $1 $2 | grep -E "^<"`
-    changes_from_second=`diff $1 $2 | grep -E "^>"`
-
-    if [[ "$changes_from_first" && "$changes_from_second" ]]; then
-        echo "BOTH CHANGED: $1 <-> $2"
-    elif [[ "$changes_from_first" ]]; then
-        gio trash $2
-        cp --verbose $1 $2
-    elif [[ "$changes_from_second" ]]; then
+function sync_files() {
+    if [[ `git status --short | grep $1` ]]; then
+        echo $1 "modified not commited"
+    elif [[ `diff $1 $2` ]]; then
         gio trash $1
         cp --verbose $2 $1
     fi
 }
 
-sync_two_files ./gtk.css ~/.config/gtk-3.0/gtk.css
-sync_two_files ./gnome-shell.css ~/.config/gnome-shell/gnome-shell.css
+sync_files gtk.css ~/.config/gtk-3.0/gtk.css
+sync_files gnome-shell.css ~/.config/gnome-shell/gnome-shell.css
