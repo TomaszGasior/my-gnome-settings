0ab2ac55d8fff1859abd61f0686a00361e5d8d92
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2021-06-11 20:45:49 +0200

    Login screen: disable touchpad tap to click

--- login-screen.sh
+++ login-screen.sh
@@ -5,8 +5,6 @@ gsettings set org.gnome.desktop.interface clock-show-seconds true
 gsettings set org.gnome.desktop.interface clock-show-weekday true
 gsettings set org.gnome.desktop.interface show-battery-percentage true
 
-gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
-
 echo '<fontconfig><match target="font"><edit mode="assign" name="antialias"><bool>true</bool></edit></match><match target="font"><edit mode="assign" name="rgba"><const>rgb</const></edit></match><match target="font"><edit mode="assign" name="hinting"><bool>true</bool></edit></match><match target="font"><edit mode="assign" name="hintstyle"><const>hintfull</const></edit></match></fontconfig>' \
     > $HOME/.config/fontconfig/conf.d/10-antialiasing.conf
 echo '<fontconfig><match target="font"><edit mode="assign" name="lcdfilter"><const>lcddefault</const></edit></match></fontconfig>' \
