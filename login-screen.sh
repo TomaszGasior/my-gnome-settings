ee408eace135cf7fc1d57a1ba5c921918eb3dc23
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2022-01-06 04:30:09 +0100

    Remove login-screen.sh

--- login-screen.sh
+++
@@ -1,5 +0,0 @@
-# run these commands from `machinectl shell gdm@.host /bin/sh`
-
-gsettings set org.gnome.desktop.interface clock-show-date true
-gsettings set org.gnome.desktop.interface clock-show-seconds true
-gsettings set org.gnome.desktop.interface clock-show-weekday true
